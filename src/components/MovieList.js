c8a22e91b8d2ff9b29d70c7e608d89377fe54036
Karthik Mathrubootham <KarthikM_Mathrubootham@bergdorfgoodman.com>
2021-06-14 17:02:34 -0400

    Pulling from omdb api

---
+++ src/components/MovieList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MovieList = (props) => {
+  return (
+    <>
+      {props.movies.map((movie, index) => (
+        <div className="image-container d-flex justify-content-start col-md-3">
+          <figcaption>{movie.Title}</figcaption>
+          <figcaption>{movie.Year}</figcaption>
+          <img src={movie.Poster} alt="movie"></img>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default MovieList;
