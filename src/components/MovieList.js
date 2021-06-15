06ee01b59188df9c3a806157c65cb3546012fdf9
Karthik Mathrubootham <KarthikM_Mathrubootham@bergdorfgoodman.com>
2021-06-14 21:02:32 -0400

    Overlay and Zoom effect on Hover

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,10 +4,12 @@ const MovieList = (props) => {
   return (
     <>
       {props.movies.map((movie, index) => (
-        <div className="image-container d-flex justify-content-start col-md-4">
-          <figcaption>{movie.Title}</figcaption>
-          <figcaption>{movie.Year}</figcaption>
+        <div className="image-container d-flex justify-content-start m-3">
+          <figcaption>{movie.title}</figcaption>
           <img src={movie.Poster} alt="movie"></img>
+          <div className="overlay d-flex align-items-center justify-content-center">
+            Add to Favourites
+          </div>
         </div>
       ))}
     </>
