7889673d724c567e0b928d165e510ce06302ce58
Karthik Mathrubootham <KarthikM_Mathrubootham@bergdorfgoodman.com>
2021-07-11 19:43:39 -0400

    troubleshoot last step

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
     );
 
     setFavourites(movieFavourites);
-  }, []);
+  }, [movieFavourites]);
 
   /* const saveToLocalStorage = (items) => {
     localStorage.setItem("react-movie-app-favourites", JSON.stringify(items));
